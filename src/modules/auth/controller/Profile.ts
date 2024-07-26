c1639838af120c6749662170a84086ff4884d29f
Nisar Khan <khannisarnk0786@gmail.com>
2024-07-26 17:00:35 +0530

    update profile

--- src/modules/auth/controller/Profile.ts
+++ src/modules/auth/controller/Profile.ts
@@ -6,10 +6,9 @@ export const profileDetails = async (req: Request, res: Response) => {
 	try {
 		const { _id } = req.user;
 		const user = await Auth.findById(_id).select('-password -resetPasswordToken')
-		if (!user) {
+		if (user.isVerified !== true) {
 			return res.status(400).json({ error: 'You are not authenticated to access this info..' })
-		}
-		return res.status(200).json({ data: user })
+		} else return res.status(200).json({ data: user })
 	} catch (error) {
 		return res.status(500).json({ error: error.message });
 	}
